323c006c5c8bc7886e068456529a3d117a487122
xieJialun <36151536+xjryanse@users.noreply.github.com>
2026-01-22 01:00:30 +0800

    1

--- src/service/DbService.php
+++ src/service/DbService.php
@@ -24,7 +24,9 @@ class DbService {
     public static function dbConf($dbSource){
         $md5RPH = md5(ROOT_PATH);
         // 为了兼容cli下使用，增加md
-        $host = $_SERVER['SERVER_NAME'] ?: $md5RPH;
+        $host = isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] 
+                ? $_SERVER['SERVER_NAME'] 
+                : $md5RPH;
         $info = EntrySdk::hostBindInfo($host);
         if(!$info){
             
